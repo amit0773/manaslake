ede51f906198eb4203d2c4d5b6a628fd99cfccaa
amit adav <amit@ajency.in>
2014-09-26 18:03:15 +0530

    menu conflict

--- wp-content/themes/HighendWP-child/scripts/jquery.child.js
+++ wp-content/themes/HighendWP-child/scripts/jquery.child.js
@@ -2,13 +2,19 @@ var $j = jQuery.noConflict();
 // custom js
 $j(document).ready(function(){
 	$j('.menu-item-has-children').on('click', function(e) {
-		e.preventDefault();
-		$j(this).find('ul.sub-menu').toggle();
-		if($j(this).find('ul.sub-menu').is(':hidden')){
-		$j(this).find('ul.sub-menu').parent().addClass("uparrow").removeClass('downarrow');
-		}else{
-		$j(this).find('ul.sub-menu').parent().addClass("downarrow").removeClass('uparrow'); 
+		//e.preventDefault();
+		
+		console.log($j(e.target).attr('href'))
+			if( $j(e.target).attr('href')  && $j(e.target).attr('href') == '#' ) {
+			$j(this).find('ul.sub-menu').toggle();
+			if($j(this).find('ul.sub-menu').is(':hidden')){
+			$j(this).find('ul.sub-menu').parent().addClass("uparrow").removeClass('downarrow');
+			}else{
+			$j(this).find('ul.sub-menu').parent().addClass("downarrow").removeClass('uparrow'); 
 		}
+}else{
+return;
+	}
 		
 	});
 });
