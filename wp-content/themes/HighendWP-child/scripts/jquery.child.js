0014c362dd27e1c6a374887d9ed31b0e3b4be6ba
amit adav <amit@ajency.in>
2014-10-29 16:10:41 +0530

    query not working

--- wp-content/themes/HighendWP-child/scripts/jquery.child.js
+++ wp-content/themes/HighendWP-child/scripts/jquery.child.js
@@ -29,7 +29,7 @@ return;
 	
         $j(".contact").click(function () {
             $j("#scrolltriggered").show();
-	$j('#scrolltriggered').css({bottom:'0px',display:'block'});
+	$j('#scrolltriggered').css({bottom:'0px',visibility:'visible'});
         });
 
    
